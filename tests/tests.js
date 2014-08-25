f7fc6fa1108c9585a858b6d029c121a36717b9af
Riccardo Marton <marton.riccardo@gmail.com>
2014-08-25 19:32:05 +0200

    Added page adding and navigation test units

--- tests/tests.js
+++ tests/tests.js
@@ -4,15 +4,37 @@
 
 app = lightly();
 
+var container = document.getElementById('lightly-container');
+app.setContainer(container);
+
 /**
  *	General tests
  */
 
-QUnit.test( "app name", function( assert ) {
+QUnit.test( "change appname", function( assert ) {
 
 	var newname = 'aaaa';
 	app.setAppName(newname);
 	var name = app.getAppName();
-	assert.ok( name == newname, 'Setted "'+newname+'" as app name with success' );
+	assert.ok( name == newname, 'Setted "'+newname+'" as app name' );
 
+});
+
+QUnit.test( "page managing and navigation", function( assert ) {
+
+	//add page
+	var page_test = {
+		id: 'test',
+		title: 'page test',
+		contents: {
+			quote: "this is a test!" 
+		}
+	}
+	app.addPage(page_test);
+	var pages = app.getPages();
+	assert.ok( typeof pages['test'] != "undefined", 'Added test page' );
+
+	//navigate
+	app.navigate("test");
+	assert.ok( document.getElementById('quote').innerHTML == page_test.contents.quote, 'Navigation to test page' );
 });
